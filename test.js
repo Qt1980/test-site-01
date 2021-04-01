22e1309cb640aa381451883fe45ab85c459a1aab
Qadree Trimble <qadree_t@yahoo.com>
2021-03-31 18:47:54 -0700

    upadted lines with meanings

--- test.js
+++ test.js
@@ -15,8 +15,16 @@ myForm.addEventListener('submit', event => {
   console.log('submitted');
 });
 
+//line 19 and 20: creates a variable named nameEl and nameHeadeEl that contains the value of the "name" and "nameHeader" property in html.
+const nameEl = document.getElementById('name');
+const nameHeaderEl = document.getElementById('nameHeader');
+//line 22: creates an event listener and attaches it to the "nameEl" variable. The event listener is using 'input' which is a property of the listener that tracks every change made inside the text field on a form.
+nameEl.addEventListener('input', event => {
+  nameHeaderEl.innerText = `Welcome, ${event.target.value}!`;
+});
+
 myForm.addEventListener('submit', handleFormSubmitted);
 
 let nums = [5, 3, 9, 2, 1];
-let results = nums.map(num => num * 3);
+let results = nums.map(num => num * 3); //map prototype displays the results of a function in a new array that touches all elements or indexes from the origin array
 console.log(results);
